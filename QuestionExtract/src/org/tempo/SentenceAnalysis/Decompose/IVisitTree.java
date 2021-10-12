46965553f9b22c4b6756d205aa9a76f04ec7e7aa
Zhen Jia <zjia@mpi-inf.mpg.de>
2021-10-12 11:14:38 +0200

    Uploaded tequila frontend code

--- QuestionExtract/src/org/tempo/SentenceAnalysis/Decompose/IVisitTree.java
+++
@@ -1,5 +0,0 @@
-package org.tempo.SentenceAnalysis.Decompose;
-
-public interface IVisitTree<TreeNode extends Object> {
-	void Visit(TreeNode node);
-}
--- QuestionExtract/src/org/tempo/SentenceAnalysis/EnumTagType.java
+++
@@ -1,4 +0,0 @@
-package org.tempo.SentenceAnalysis;
-
-public enum EnumTagType{Unkown, DateFromRule, OrdinalFromRule, 
-						TimeHeilderTime, TimeSUTime, NEREntity, Event, When}
